14af023f84187f91cb3512d888db0d1c0d510bb9
codermahem691111 <70892387+codermahem691111@users.noreply.github.com>
2023-04-15 12:01:54 +0600

    commitinghomepage

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import { Cards } from './Cards';
+import { Footer } from './Footer';
+import { Carousel } from './Carousel';
+import {History} from './History';
+import { Team } from './Team';
+import {Doctors} from './Doctors'
+import { How } from './How';
+import { Dsc } from './Dsc';
+import { Testo } from './Testo';
+import  {Books} from './Books'
+import { About } from './pages/About';
+import {Faq}  from './pages/Faq'
+import { Contact } from './pages/Contact';
+import { Videotes } from './pages/Videotes';
+import ReactDOM from "react-dom/client";
+import { Link, Routes, Route } from 'react-router-dom';
+import { Navbar } from './Navbar';
+import  {Navbar2} from './Navbar2';
+import { Study } from './Study';
+import { Shop } from './Shop';
+import { Blogs } from './Blogs';
+function App() {
+  return (
+  <>
+  <h1 className='text-3xl text-center text-blue-200'>hellonavbar</h1>  
+  <Navbar></Navbar>
+  <Carousel></Carousel>
+  <Cards></Cards>
+  <History></History>
+  <Team></Team>
+  <Doctors></Doctors>
+ <How></How>
+ <Dsc></Dsc>
+ <Testo></Testo>
+ <Books></Books>
+ <Study></Study>
+ <Shop></Shop>
+ <Blogs></Blogs>
+ <Footer></Footer>
+
+ </>
+
+  );
+}
+
+export default App;
